a35fa1a03577a5a53905ac965f9d7d1a99cafdb9
Adnangad <gardobuyaadnan@gmail.com>
2023-11-15 08:45:32 -0800

    task-1

--- main.c
+++ main.c
@@ -3,42 +3,83 @@
  * main - Entry point for the simple shell.
  * Return: Always 0.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
     char *input = NULL;
     char *token[MAX_TOKENS];
     char *envp[] = {NULL};
     ssize_t len;
 
-    while (1)
+    if (argc > 1)
     {
-        len = read_input(&input);
-
-        if (len == -1)
+        FILE *file = fopen(argv[1], "r");
+        if (!file)
         {
-            break;
+            perror("Error opening file\n");
+            exit(EXIT_FAILURE);
         }
 
-        if (strcmp(input, "exit") == 0)
+        while ((len = getline(&input, NULL, file)) != -1)
         {
-            break;
-        }
-
-        tokenize_input(input, token);
-
-        if (token[1] != NULL)
-        {
-            execute_command_with_arguments(token);
-        }
-        else
-        {
-            if (strchr(token[0], '/') == NULL)
+            if (len > 0 && input[len - 1] == '\n')
             {
-                execute_command_simple(token);
+                input[len - 1] = '\0';
+                len--;
+            }
+
+            tokenize_input(input, token);
+
+            if (token[1] != NULL)
+            {
+                execute_command_with_arguments(token);
             }
             else
             {
-                execute_command_with_path(token, envp);
+                if (strchr(token[0], '/') == NULL)
+                {
+                    execute_command_simple(token);
+                }
+                else
+                {
+                    execute_command_with_path(token, envp);
+                }
+            }
+        }
+
+        fclose(file);
+    }
+    else
+    {
+        while (1)
+        {
+            len = read_input(&input);
+
+            if (len == -1)
+            {
+                break;
+            }
+
+            if (strcmp(input, "exit") == 0)
+            {
+                break;
+            }
+
+            tokenize_input(input, token);
+
+            if (token[1] != NULL)
+            {
+                execute_command_with_arguments(token);
+            }
+            else
+            {
+                if (strchr(token[0], '/') == NULL)
+                {
+                    execute_command_simple(token);
+                }
+                else
+                {
+                    execute_command_with_path(token, envp);
+                }
             }
         }
     }
